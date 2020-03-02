55146c6cb79c43cba7ae37f93fc3a9c2a4b08f07
Einstein Olivo <einsteinolivo@gmail.com>
2020-03-02 00:52:04 -0400

    Adding to admin

--- purchase/admin.py
+++ purchase/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Market, Item, Purchase, Item_purchase
 # Register your models here.
+
+admin.site.register(Market)
+admin.site.register(Item)
+admin.site.register(Purchase)
+admin.site.register(Item_purchase)
